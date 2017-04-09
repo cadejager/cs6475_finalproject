919e1e5f640a649ca9759ecbed24feb9204a3842
CGearhart <cgearhart@gmail.com>
2017-04-08 21:34:01 -0400

    Remove bonnie submodule and update submit to use nelson package

--- submit.py
+++ submit.py
@@ -1,109 +1,82 @@
-import time
 import os
-import sys
 import argparse
-import json
-import datetime
-from bonnie.submission import Submission
 
-# open stdout unbuffered to automatically flush prints
-sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+import nelson.gtomscs as nelson
 
-FILENAMES = {'finalproject': ["finalproject.pdf"]}
-SIZE_LIMIT = 6       # maximum size of each file uploaded
-REFRESH_TIME = 3.0   # number of seconds to wait during results polling
+from glob import glob
 
-LATE_POLICY = \
-"""Late Policy:\n
+COURSE = "cs6475"
+SETTINGS = {'finalproject': {"finalproject": {"size": 6, "ext": ["pdf"]}}}
+
+LATE_POLICY = """Late Policy:\n
   \"I have read the late policy for CS6475. I understand that only my last
   commit before the late submission deadline will be accepted and that late
   penalties apply if any part of the assignment is submitted late.\"
 """
 
-HONOR_PLEDGE = "Honor Pledge:\n\n  \"I have neither given nor received aid on this assignment.\"\n"
+HONOR_PLEDGE = """Honor Pledge:\n
+  \"I have neither given nor received aid on this assignment.\"
+"""
 
 
-def require_pledges():
-
-    print(LATE_POLICY)
+def require_pledge(policy_text):
+    print(policy_text)
     ans = raw_input("Please type 'yes' to agree and continue>")
     if ans != "yes":
-        raise RuntimeError("You must accept the late policy to submit your assignment.")
-    print
-
-    print(HONOR_PLEDGE)
-    ans = raw_input("Please type 'yes' to agree and continue>")
-    if ans != "yes":
-        raise RuntimeError("You must accept the honor pledge to submit your assignment.")
+        raise RuntimeError(
+            "You accept all policies before submitting your assignment.")
     print
 
 
-def check_files(filenames):
+def validate_file_info(pattern, lo=1, hi=1, size=6, ext=[]):
 
-    if not all(map(lambda x: os.path.isfile(x), filenames)):
-        raise RuntimeError("Submission Failed: One or more required files " +
-                           "is missing from the current directory. Make " +
-                           "sure all these files exist: {!s}".format(filenames))
-    elif any(map(lambda x: os.stat(x).st_size >> 20 > SIZE_LIMIT, filenames)):
-        large_files = filter(lambda x: os.stat(x).st_size >> 20 > SIZE_LIMIT, filenames)
-        raise RuntimeError("Submission Failed: One or more required files is " +
-                           "too large. Please make sure that the following " +
-                           "files are under {}MB and try again: {!s}".format(SIZE_LIMIT, large_files))
+    filenames = [x for x in glob(pattern + ".*")
+                 if not ext or str.lower(os.path.splitext(x)[-1])[1:] in ext]
+
+    if not (lo <= len(filenames) <= hi):
+        raise RuntimeError(
+            ("Submission Failed - you are required to submit at least {!s} " +
+             "files and no more than {!s} files that match the pattern " +
+             "'{!s}.<EXT>' with one of the allowed extensions: {!s}. You " +
+             "submitted only {!s}")
+            .format(lo, hi, pattern, ext, len(filenames)))
+
+    large_files = [x for x in filenames if os.stat(x).st_size > size * 2**20]
+    if large_files:
+        raise RuntimeError(
+            ("Submission Failed: One or more files is too large. Please " +
+             "make sure that the following files are under {!s}MB and try " +
+             "again: {!s}").format(size, large_files))
+
+    return filenames
 
 
 def main(args):
 
-    check_files(args.filenames)
+    require_pledge(LATE_POLICY)
+    require_pledge(HONOR_PLEDGE)
 
-    require_pledges()
+    required_files = [validate_file_info(ptn, **kwargs)
+                      for ptn, kwargs in SETTINGS.get(args.quiz, {}).items()]
+    additional_files = map(glob, args.filenames)
+    filenames = reduce(list.__add__, required_files + additional_files, [])
 
-    print "Submitting files..."
-    submission = Submission('cs6475', args.quiz,
-                            filenames=args.filenames,
-                            environment=args.environment,
-                            provider=args.provider)
-
-    print "\nWaiting for results..."
-    while not submission.poll():
-        print "    Refresh in {} seconds...".format(REFRESH_TIME)
-        time.sleep(REFRESH_TIME)
-    print "    Done!"
-
-    print "\nResults:\n--------"
-    if submission.feedback():
-
-        if 'message' in submission.feedback():
-            print submission.feedback()['message']
-
-        timestamp = "{:%Y-%m-%d-%H-%M-%S}".format(datetime.datetime.now())
-        filename = "%s-result-%s.json" % (args.quiz, timestamp)
-
-        with open(filename, "w") as fd:
-            json.dump(submission.feedback(), fd, indent=4, separators=(',', ': '))
-
-        print("\n(Details available in %s)\n" % filename)
-
-    elif submission.error_report():
-        error_report = submission.error_report()
-        print(json.dumps(error_report, indent=4))
-
-    else:
-        print("Unknown error.")
+    nelson.submit(COURSE, args.quiz, filenames, environment=args.environment)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Submits code to the Udacity site.')
-    parser.add_argument('quiz', choices=FILENAMES.keys(),
-                        help="Select the name of the quiz to submit")
-    parser.add_argument('-f', '--filenames', nargs='+', default=[],
-                        help="Names of additional files to include with submission " + \
-                        "(finalproject.pdf must always be included in your submission)")
-    parser.add_argument('--provider', choices=['gt', 'udacity'], default='gt',
-                        help="Select the authentication system to use (default: gt)")
-    parser.add_argument('--environment', default='production',
-                        choices=['local', 'development', 'staging', 'production'],
-                        help="Select the server to use (default: production)")
+    parser = argparse.ArgumentParser(
+        description='Submit assignment to the Bonnie.')
+    parser.add_argument(
+        'quiz', choices=SETTINGS.keys(),
+        help="Select the name of the quiz to submit")
+    parser.add_argument(
+        '-f', '--filenames', nargs="+", default=[],
+        help="The names of any additional files to submit.")
+    parser.add_argument(
+        '-e', '--environment', default='production',
+        choices=['local', 'development', 'staging', 'production'],
+        help="Select the server to use (default: production)")
     args = parser.parse_args()
-    args.filenames += FILENAMES[args.quiz]
 
     main(args)
